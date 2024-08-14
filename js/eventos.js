21a651ab1b587671650078d4b2b14cdfbda0e9fd
Avril Albistur <aalbistur27@gmail.com>
2024-08-14 14:08:24 -0300

    Solucion del btn

--- js/eventos.js
+++ js/eventos.js
@@ -1,5 +1,10 @@
 let div = document.getElementById('div');
 
 div.addEventListener('click', () => {
-    alert('Hola! Soy el div')
-})
+    alert('Hola! Soy el div');
+});
+
+function saludar(event) {
+    event.stopPropagation(); // Detiene la propagación del evento hacia el div
+    alert('Hola!');
+}
